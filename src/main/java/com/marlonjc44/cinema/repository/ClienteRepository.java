48e3ad8cc78e02b719d0bf57dd1e2725314e5b19
m0cruz <marlonjc44@gmail.com>
2026-05-08 15:23:38 -0300

    Controller e Repository Cliente

---
+++ src/main/java/com/marlonjc44/cinema/repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package com.marlonjc44.cinema.repository;
+
+import com.marlonjc44.cinema.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository  extends JpaRepository <Cliente, Long>{
+
+}
